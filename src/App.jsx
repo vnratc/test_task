4d582c20a2218b3f990f5c8e01d7ec32aa130c9b
vnratc <105481726+vnratc@users.noreply.github.com>
2023-05-14 23:11:17 +0400

    wip comments and breakdown into files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 import BarChart from './BarChart'
+import UserCards from './UserCards'
 
 
+// App component.
 function App() {
+
+  // State for fetched and processed data.
   const [todosByUser, setTodosByUser] = useState([])
+
+  // State for button and preloader.
   const [loading, setLoading] = useState({ btnDisabled: false, preloader: "" })
 
   async function fetchItems(e) {
+
+    // Disable button and show preloader.
     setLoading({ btnDisabled: true, preloader: "preloader" })
+
+    // Prevent default form submission behaviour.
     e.preventDefault()
+
+    // Fetch todos from url.
     await fetch("https://jsonplaceholder.typicode.com/todos")
       .then(res => res.json())
       .then(data => {
-        // Group all data by userId
+
+        // Store todos grouped by userId in groupedByUser.
         const groupedByUser = data.reduce((groupedUsers, obj) => {
           const userId = obj.userId
           if (!groupedUsers[userId]) groupedUsers[userId] = []
@@ -21,50 +34,50 @@ function App() {
           return groupedUsers
         }, []).slice(1)
 
-        // Create obj with "array of users" and "completed" count
+        // Create object with arrays of users and completed/notCompleted count.
         const groupedWithCompleted = groupedByUser.map(user => {
           const completed = user.filter(todo => todo.completed).length
           const notCompleted = user.filter(todo => !todo.completed).length
           return { data: [...user], completed, notCompleted }
         });
 
-        // Sort users
+        // Store sorted arrays of users by completed in descending order in sorted.
         const sorted = groupedWithCompleted.toSorted((a, b) => {
           return b.completed - a.completed
         })
+
+        // Set state for todosByUser.
         setTodosByUser(sorted)
       })
+
+    // Enable button and hide preloader.
     setLoading({ btnDisabled: false, preloader: "" })
   }
 
 
-  let todoItems = []
-  const userItems = todosByUser.map((user, i) =>
-    <div className='card' key={`user${user.data[i].userId}`}>
-      <div className='card-title'>
-        <span>User {user.data[i].userId}</span>
-        <span>
-          <span id='completed'>{user.completed}</span> /  
-          <span id='notCompleted'> {user.notCompleted}</span>
-        </span>
-      </div>
-      <ul>{todoItems = user.data.map(todo =>
-        <li key={todo.id}>
-          {todo.title}
-        </li>
-      )}</ul>
-    </div>
-  )
+  // Retrun components.
+
 
   return (
     <>
       <div id='main'>
-      <form id='form' onSubmit={fetchItems}>
-        <button autoFocus={true} disabled={loading.btnDisabled} id="btn">Fetch Data</button>
-        <div className={loading.preloader}></div>
-      </form>
-      <div id='container'>{userItems}</div>
-      <BarChart todosByUser={todosByUser}/>
+
+        {/* Button. */}
+        <form id='form' onSubmit={fetchItems}>
+          <button autoFocus={true} disabled={loading.btnDisabled} id="btn">Fetch Data</button>
+
+          {/* Preloader */}
+          <div className={loading.preloader}></div>
+        </form>
+
+        {/* User cards. */}
+        <div id='container'>
+          <UserCards todosByUser={todosByUser} />
+        </div>
+
+        {/* Bar Chart */}
+        <BarChart todosByUser={todosByUser} />
+
       </div>
     </>
   )
